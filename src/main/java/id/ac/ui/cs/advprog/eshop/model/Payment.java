bdca861fdfeb363dcea05e54aebe31a190023b79
unknown <playarisa24@gmail.com>
2026-03-10 02:04:35 +0700

    feat: [GREEN] implement payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Builder
@@ -14,8 +15,28 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        if (id == null || method == null || paymentData == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] statusList = {"SUCCESS", "FAILED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
     }
 
     public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "FAILED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
